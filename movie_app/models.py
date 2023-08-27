026e46840bd85704709c8d60e19461202e868ac6
SalavatSM <salavatfromkg@gmail.com>
2023-08-27 16:00:19 +0600

    HW_2. Add 'stars' field to 'Review' model. Add func to display list of reviews and average score. Add func to display Directors with movie count.

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Avg
 
 
 class Director(models.Model):
@@ -7,21 +8,47 @@ class Director(models.Model):
     def __str__(self):
         return self.name
 
+    # @property
+    # def movies_count(self):
+    #     return 2+3
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     duration = models.FloatField(default=0)
-    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, related_name='movies')
 
     def __str__(self):
         return self.title
 
+    @property
+    def director_name(self):
+        return self.director.name if self.director else 'No director.'
+
+    @property
+    def rating(self):
+        return self.reviews.aggregate(avg_rating=Avg('stars'))['avg_rating']
+
+    # @property
+    # def movies_count(self):
+    #     return sum(self.movie)
+
 
 class Review(models.Model):
+    movie = models.ForeignKey('Movie', on_delete=models.CASCADE, related_name='reviews')
     text = models.TextField(null=True, blank=True)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    stars = models.IntegerField(
+        choices=(
+            (1, 1),
+            (2, 2),
+            (3, 3),
+            (4, 4),
+            (5, 5),
+        ), default=1
+    )
 
     def __str__(self):
-        return self.text
+        return self.movie.title
 
--- movie_app/serializers.py
+++ movie_app/serializers.py
@@ -2,22 +2,30 @@ from rest_framework import serializers
 from .models import Director, Movie, Review
 
 
-class DirectorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Director
-        fields = '__all__'
-
-
 class MovieSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = 'id title description duration director_name rating'.split()
+
+
+class DirectorSerializer(serializers.ModelSerializer):
+    movies_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Director
+        fields = 'name movies_count'.split()
+
+    def get_movies_count(self, obj):
+        return obj.movies.count()
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    movie = MovieSerializer()
+
     class Meta:
         model = Review
-        fields = '__all__'
+        fields = 'movie text stars'.split()
 
 
 
